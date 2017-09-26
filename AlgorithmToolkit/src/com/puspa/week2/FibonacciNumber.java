b6701c65061d5f3eb147e8518b256048d8b4704a
puspakhanal <puspakhanal@users.noreply.github.com>
2017-09-25 22:03:54 -0400

    Efficient Algorithm using Euclidean algorithm

--- AlgorithmToolkit/src/com/puspa/week2/FibonacciNumber.java
+++ AlgorithmToolkit/src/com/puspa/week2/FibonacciNumber.java
@@ -1,16 +1,25 @@
 package com.puspa.week2;
 
+import java.util.Scanner;
+
 public class FibonacciNumber {
 	
-	public int getFibonacciNumber(int n) {
-		int[] fibArray = new int[n+1];
+	public static int getFibonacciNumber(int n) {
+		if(n < 2) return n;
+		int fibArray[] = new int[n+1];
 		fibArray[0] = 0;
 		fibArray[1] = 1;
 		
-		for (int i = 0; i <= n; i++) {
+		for (int i = 2; i <= n; i++) {
 			fibArray[i] = fibArray[i-1] + fibArray[i-2];
 		}
 		return fibArray[n];
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.println(getFibonacciNumber(n));
 	}
 
 }
