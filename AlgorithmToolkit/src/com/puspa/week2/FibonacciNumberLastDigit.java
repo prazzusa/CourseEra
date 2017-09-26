b69a9f95b7cfe721e504a177caa2198ecfeaedad
puspakhanal <puspakhanal@users.noreply.github.com>
2017-09-26 00:38:15 -0400

    Bug Fixes

--- AlgorithmToolkit/src/com/puspa/week2/FibonacciNumberLastDigit.java
+++ AlgorithmToolkit/src/com/puspa/week2/FibonacciNumberLastDigit.java
@@ -3,22 +3,22 @@ package com.puspa.week2;
 import java.util.Scanner;
 
 public class FibonacciNumberLastDigit {
-	
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		System.out.println(getLastDigitOfFibonacciNumber(n));
 	}
-	
-	public static int getLastDigitOfFibonacciNumber(int n) {
-		int fibArray[] = new int[n+1];
+
+	public static long getLastDigitOfFibonacciNumber(int n) {
+		long fibArray[] = new long[n+1];
 		fibArray[0] = 0;
 		fibArray[1] = 1;
-		
+
 		for (int i = 2; i <= n; i++) {
-			fibArray[i] = fibArray[i-1] + fibArray[i-2];
+			fibArray[i] = (fibArray[i-1] + fibArray[i-2]) % 10;
 		}
-		return fibArray[n] % 10;
+		return fibArray[n];
 	}
 
 }
